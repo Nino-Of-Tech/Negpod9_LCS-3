2dadaf3d0c2f3cfc825bfbc75f2524a70c02eb7b
Freedisch <82499435+Freedisch@users.noreply.github.com>
2022-07-18 14:57:31 +0200

    Ver2

---
+++ if-statements2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+Months1 = ['January', 'March', 'May', 'July', 'August', 'October', 'November']
+Months2 = ['February']
+Months3 = ['April', 'June',  'September', 'December']
+Month = ""
+while(Month == "" ):
+    Month = input("Enter the months:")
+    if Month in Months1:
+        print(f"The month of {Month} has 31 days.")
+        break
+    if Month in Months3:
+        print(f"The month of {Month} has 30 days")
+        break
+    if Month in Months2:
+        print(f"The month of {Month} has 28 days.")
+        break
+    print("Input incorrect")
+    Month = " "
